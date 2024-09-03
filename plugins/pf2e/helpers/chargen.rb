9ea920878291192710212bd3eb707aac93983ba9
John Madon <john@unwisegeek.net>
2024-09-02 23:58:40 -0400

    Added abilities to restore_checkpoint

--- plugins/pf2e/helpers/chargen.rb
+++ plugins/pf2e/helpers/chargen.rb
@@ -685,7 +685,11 @@ module AresMUSH
           # Write the character object
           char.save
         when "abilities"
+          restore_checkpoint(char, "info")
+          char.pf2_boosts_working = checkpoint_info["abilities"]["pf2_boosts_working"]
 
+          char.chargen_stage = "6"
+          char.save
         when "skills"
         else
           return nil
