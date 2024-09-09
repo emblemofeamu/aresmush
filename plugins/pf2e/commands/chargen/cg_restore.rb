df0b3de47947f33ec09d7cf3aa7b7a77dafeec45
John Madon <john@unwisegeek.net>
2024-09-08 21:52:01 -0400

    New logic for cg/restore command

--- plugins/pf2e/commands/chargen/cg_restore.rb
+++ plugins/pf2e/commands/chargen/cg_restore.rb
@@ -13,12 +13,12 @@ module AresMUSH
       end
 
       def check_in_chargen
-        stages = { "skills" => "6", "abilities" => "5", "info" => "4" }
+        stages = { "skills" => 6, "abilities" => 5, "info" => 4 }
         if enactor.is_approved? || enactor.chargen_locked
           return t('pf2e.only_in_chargen')
         elsif !enactor.chargen_stage
           return t('chargen.not_started')
-        elsif stages[self.checkpoint] == enactor.chargen_stage
+        elsif stages[checkpoint] > stages[enactor.pf2_checkpoint]
           return t('pf2e.cg_cant_restore_to_stage_youre_in', :checkpoint=>self.checkpoint)
         else
           return nil
