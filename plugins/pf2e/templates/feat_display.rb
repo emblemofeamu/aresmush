270c2656d06db848ef44eedbe63b21209c6ae9c7
Admin <admin@test.emblemofeamush.net>
2026-03-29 18:59:07 -0400

    feat/search pagination enhancement

--- plugins/pf2e/templates/feat_display.rb
+++ plugins/pf2e/templates/feat_display.rb
@@ -3,17 +3,25 @@ module AresMUSH
     class PF2eFeatDisplay < ErbTemplateRenderer
       include CommonTemplateFields
 
-      attr_accessor :paginator, :title
+      attr_accessor :paginator, :title, :page_notice
 
-      def initialize(paginator, title)
+      def initialize(paginator, title, page_notice = nil)
         @paginator = paginator
         @title = title
+        @page_notice = page_notice
 
         super File.dirname(__FILE__) + "/feat_display.erb"
       end
 
       def title
         @title
+      end
+
+      def page_footer
+        footer = @paginator.page_footer
+        return footer if @page_notice.blank?
+
+        "#{footer}\n#{@page_notice}"
       end
 
     end
